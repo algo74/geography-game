2ed33552381ac1d4c63415ce58229b7206efef50
Alex G <agoponenko@gmail.com>
2018-03-18 11:34:34 -0400

    made it work, fixed Chrome outline, no select on focus

--- app/components/playstring-panel.js
+++ app/components/playstring-panel.js
@@ -1,21 +1,44 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  input: null,
+  dummy: null,
+  getInput() {
+    if(!this.input) {
+      this.input = this.$('#player-input');
+    }
+    return this.input;
+  },
+  didRender() {
+    this.getInput().focus();
+
+  },
   actions: {
     handleSubmit() {
       this.get('delegateSubmit')();
       // reset input
-      let input = this.$('#player-input');
-      input.val('');
-      input.width('1px');
+      this.getInput();
+      this.input.val('');
+      this.input.width('1px');
+    },
+    handleFocus() {
+      console.log("focus in");
+      let input=this.getInput();
+      setTimeout(function() {
+        let length = input.val().length;
+        input[0].setSelectionRange(length,length);
+      }, 0);
     },
     handleInput() {
       
-      let input = this.$('#player-input');
-      let dummy = this.$('#player-input-dummy');
-      dummy.html(input.val());
-      input.width(dummy.width() || 1);
-      console.log(dummy.width() );
+      this.getInput();
+      if(!this.dummy) {
+        this.dummy = this.$('#player-input-dummy');
+      }
+      
+      this.dummy.html(this.input.val());
+      this.input.width(this.dummy.width() || 1);
+      console.log(this.dummy.width() );
     }
   }
 });
