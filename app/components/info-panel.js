135b1743e6dbba746825fbe0a8f4a2daa027a347
Alex G <agoponenko@gmail.com>
2018-07-23 10:33:15 -0400

    fix caret position in playstring

--- app/components/info-panel.js
+++ app/components/info-panel.js
@@ -4,11 +4,8 @@ import { inject as service } from '@ember/service';
 export default Component.extend({
   maps: service(),
   didRender() {
-    console.log('update map');
     this._super(...arguments);
     let city = this.get('data');
-    console.log('inside infopanel; city: ');
-    console.log(city);
     let mapElement = this.get('maps').getMapElement(city);
     this.$('.map-container').empty().append(mapElement);
   },
--- app/components/playstring-panel.js
+++ app/components/playstring-panel.js
@@ -3,6 +3,44 @@ import Component from '@ember/component';
 export default Component.extend({
   input: null,
   dummy: null,
+  init() {
+    this._super(...arguments);
+    console.log('init playstring');
+    const position = this.entered.length;
+    this.position = { start: position, end: position };
+  },
+  getCaretPosition(ctrl) {
+    // IE < 9 Support
+    if(document.selection) {
+      ctrl.focus();
+      var range = document.selection.createRange();
+      var rangelen = range.text.length;
+      range.moveStart('character', -ctrl.value.length);
+      var start = range.text.length - rangelen;
+      return { 'start': start, 'end': start + rangelen };
+    }
+    // IE >=9 and other browsers
+    else if(ctrl.selectionStart || ctrl.selectionStart == '0') {
+      return { 'start': ctrl.selectionStart, 'end': ctrl.selectionEnd };
+    } else {
+      return { 'start': 0, 'end': 0 };
+    }
+  },
+  setCaretPosition(ctrl, start, end) {
+    // IE >= 9 and other browsers
+    if (ctrl.setSelectionRange) {
+      ctrl.focus();
+      ctrl.setSelectionRange(start, end);
+    }
+    // IE < 9
+    else if (ctrl.createTextRange) {
+      var range = ctrl.createTextRange();
+      range.collapse(true);
+      range.moveEnd('character', end);
+      range.moveStart('character', start);
+      range.select();
+    }
+  },
   getInput() {
     if(!this.input) {
       this.input = this.$('#player-input');
@@ -10,8 +48,7 @@ export default Component.extend({
     return this.input;
   },
   didRender() {
-    this.getInput().focus();
-
+    this.setCaretPosition(this.getInput()[0], this.position.start, this.position.end);
   },
   actions: {
     handleSubmit() {
@@ -36,14 +73,18 @@ export default Component.extend({
       }, 0);
     },
     handleInput() {
+      // get fields
       this.getInput();
       if(!this.dummy) {
         this.dummy = this.$('#player-input-dummy');
       }
-      
+      // save caret position
+      this.position = this.getCaretPosition(this.input[0]);
+      // resize
       this.dummy.html(this.input.val());
       this.input.width(this.dummy.width() || 1);
-      //console.log(this.dummy.width() );
+      //restore caret position, in case it won't rerended...
+      this.setCaretPosition(this.input[0], this.position.start, this.position.end);
     }
   }
 });
